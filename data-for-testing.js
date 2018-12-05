33b77e7ebcc7dc96f970e04529526f7c8b69ac27
julien demarque <julien_demarque@hotmail.fr>
2018-12-05 02:21:25 +0200

    added testing

---
+++ data-for-testing.js
@@ -0,0 +1,23 @@
+export const ProgressBarProps = {
+	colorgiven: '#3b3346',
+	progress: 0.2
+};
+
+export const SpeedControlProps = {
+	speed: 1
+};
+
+export const TimeDisplayProps = {
+	time: 20,
+	duration: 300
+};
+
+export const VideoPlayerProps = {
+	width: '100%',
+	source: 'www.mymockurl.com/mymockvideo.mp4',
+	color: '#3b3346'
+};
+
+export const VolumeControlProps = {
+	volume: 0.5
+};
